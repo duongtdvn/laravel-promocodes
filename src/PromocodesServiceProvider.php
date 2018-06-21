b1e9c88fdb14f66b79b95cc10bd26a579d25cdb0
truongduong <duongtd@sololab.net>
2018-06-21 09:27:06 +0700

    fix assets publish directory path

--- src/PromocodesServiceProvider.php
+++ src/PromocodesServiceProvider.php
@@ -15,18 +15,18 @@ class PromocodesServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '../config/promocodes.php' => config_path('promocodes.php')
+            __DIR__ . '/../config/promocodes.php' => config_path('promocodes.php')
         ], 'config');
 
         $this->publishes([
-            __DIR__ . '../database/migrations/2018_06_20_000000_create_promocodes_table.php'
+            __DIR__ . '/../database/migrations/2018_06_20_000000_create_promocodes_table.php'
         ], 'migrations');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '../config/promocodes.php', 'promocodes'
+            __DIR__ . '/../config/promocodes.php', 'promocodes'
         );
 
         $this->app->singleton(Promocodes::class, function ($app) {
