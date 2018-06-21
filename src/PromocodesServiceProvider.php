1527b30c973b55425d069e8c397de13f3feb5c27
truongduong <duongtd@sololab.net>
2018-06-21 09:43:51 +0700

    improve service provider

--- src/PromocodesServiceProvider.php
+++ src/PromocodesServiceProvider.php
@@ -14,19 +14,27 @@ class PromocodesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../config/promocodes.php' => config_path('promocodes.php')
-        ], 'config');
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+
+        if (! class_exists('CreatePromocodesTable')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_promocodes_table.php' => database_path('migrations/'.$timestamp.'_create_promocodes_table.php')
+            ], 'migrations');
+        }
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/2018_06_20_000000_create_promocodes_table.php'
-        ], 'migrations');
+            __DIR__.'/../config/promocodes.php' => config_path('promocodes.php')
+        ], 'config');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/promocodes.php', 'promocodes'
+            __DIR__.'/../config/promocodes.php', 'promocodes'
         );
 
         $this->app->singleton(Promocodes::class, function ($app) {
